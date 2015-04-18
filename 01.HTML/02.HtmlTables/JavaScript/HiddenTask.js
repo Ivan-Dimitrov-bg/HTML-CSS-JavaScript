0e4cb2ae9d2529c06332c5606867d81768ec8f1f
Ivan-Dimitrov-bg <Ivan.Dimitrov.bg@gmail.com>
2015-04-19 00:18:13 +0300

    HTML
    HTML

---
+++ 01.HTML/02.HtmlTables/JavaScript/HiddenTask.js
@@ -0,0 +1,13 @@
+﻿function showHide() {
+    var checkbox = document.getElementById("chk");
+    var hiddeninputs = document.getElementsByClassName("hidden");
+
+    for (var i = 0; i != hiddeninputs.length; i++) {
+        if (checkbox.checked) {
+            hiddeninputs[i].style.display = "block";
+        }
+        else {
+            hiddeninputs[i].style.display = "none";
+        }
+    }  
+}
