dca8254d08de321f00faa5607dd4265212d34719
Ivan-Dimitrov-bg <Ivan.Dimitrov.bg@gmail.com>
2015-04-19 00:21:30 +0300

    CSS
    CSS

---
+++ 02.CSS/01.CSSOverview/javascript/HideShow.js
@@ -0,0 +1,14 @@
+﻿function showHide() {
+	var checkbox = document.getElementById("chkkk");
+	var hiddeninputs = document.getElementsByClassName("hidden");
+
+	for (var i = 0; i != hiddeninputs.length; i++) {
+		if (checkbox.checked) {
+			hiddeninputs[i].style.display = "block";
+		}
+		else {
+			hiddeninputs[i].style.display = "none";
+		}
+
+	}
+}
